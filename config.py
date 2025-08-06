d50fdc4a22a1523f978ace79ee2e9b1018e1c881
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-08-06 13:42:33 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+DISCORD_BOT_TOKEN = 'your_discord_bot_token'
+WEBHOOK_URL = 'https://discord.com/api/webhooks/XXXX'
+GAME_UNIVERSE_ID = 'your_game_universe_id'
+BUG_EGG_ITEM_ID = 'item_id_for_bug_egg'
